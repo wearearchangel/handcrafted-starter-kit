7d43af49e21b1c3e14658db6ef6447736392b43b
Michael Mwaura <michaelwmwaura@gmail.com>
2021-10-18 22:45:14 +0300

    Rebuild the starter-kit for easier plug-and-play - Improvements to webpack file, css and js compile - Add an official stylesheet and theme - Moved spec file to handcrafted project

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+import hcf from '@wearearchangel/handcrafted';
+
+(function (appName) {
+	'use strict';
+	
+	hcf.views({
+		'home': '/'
+	}, 'selected', null, () => {
+		// Code you'd like executed on all views
+	});
+})({});
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const devServer = {
+	proxy: {
+		'/handcrafted': '?'
+	},
+	static: {
+		directory: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
+		serveIndex: true,
+		watch: true
+	},
+	historyApiFallback: true
+};
+
+const config = {
+	default: {
+		entry: [
+			'./src/scripts/main.js',
+			'./src/styles/main.scss'
+		],
+		plugins: [],
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: '/node_modules/',
+					use: []
+				},
+				{
+					test: /\.scss$/,
+					type: 'asset/resource',
+					generator: {
+						filename: 'static/styles/[name].css'
+					},
+					use: ['sass-loader']
+				}
+			]
+		},
+		output: {
+			filename: 'static/scripts/[name].js',
+			path: path.resolve(__dirname, 'dist')
+		},
+		target: 'web'
+	},
+	development: {
+		mode: 'development',
+		devtool: 'source-map',
+		devServer: devServer
+	},
+	staging: {
+		mode: 'production',
+		devtool: 'source-map',
+		devServer: devServer,
+		optimization: {
+			minimize: true,
+			minimizer: [new TerserPlugin({})]
+		}
+	},
+	production: {
+		mode: 'production',
+		optimization: {
+			minimize: true,
+			minimizer: [new TerserPlugin({})]
+		}
+	}
+};
+
+module.exports = (env, argv) => ({...config.default, ...config[`${argv.mode}`]});
