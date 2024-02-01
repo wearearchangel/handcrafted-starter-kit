1a3ada94e38d7eca9459be0077ecfe36b64cc451
Michael Mwaura <michaelwmwaura@gmail.com>
2024-02-01 13:50:27 +0300

    Clean up entry points and update old packages

---
+++ src/index.html.js
@@ -0,0 +1,70 @@
+import font from "./fonts/hubot-sans.woff2";
+
+const title = "Handcrafted Starter Kit";
+const description = "Web site created using the Handcrafted Starter Kit";
+const keywords = "Handcrafted, Starter, Kit";
+
+const Index = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+
+        <meta content="IE=edge" http-equiv="X-UA-Compatible"/>
+        <meta content="text/html; charset=UTF-8" http-equiv="Content-type">
+
+        <meta content="no" name="msapplication-tap-highlight"/>
+        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <meta content="${description}" name="description"/>
+        <meta content="${keywords}" name="keywords"/>
+
+        <link href="manifest.json" rel="manifest"/>
+        <link href="favicon.ico" rel="shortcut icon"/>
+        <link as="font" crossorigin href="${font}" rel="preload" type="font/woff2">
+
+        <title>${title}</title>
+    </head>
+
+    <body>
+        <noscript>You need to enable JavaScript to view this site.</noscript>
+        <div class="wrapper">
+            <header class="header">
+                <div class="logo">
+                    <div class="logo__img">
+                        <svg viewBox="0 0 15 25" xmlns="http://www.w3.org/2000/svg">
+                            <g data-name="Layer 2" id="Layer_2">
+                                <g data-name="Layer 1" id="Layer_1-2">
+                                    <rect height="5" width="5"/>
+                                    <polygon points="15 0 15 15 5 15 5 25 0 25 0 10 10 10 10 0 15 0"/>
+                                    <rect height="5" width="5" x="10" y="20"/>
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                    <div class="logo__text">
+                        <div class="name">Handcrafted</div>
+                        <div class="tagline">Starter Kit</div>
+                    </div>
+                </div>
+
+                <nav class="nav">
+                    <a class="nav__item" data-route="/">Home</a>
+                </nav>
+            </header>
+
+            <main class="page"></main>
+
+            <footer class="footer">
+                <div class="footer__copy">
+                    &copy; ${new Date().getFullYear()} Handcrafted Starter Kit. All Rights Reserved
+                </div>
+                <div class="footer__signature">
+                    <a href="//wearearchangel.com">Handcrafted in Kenya</a>
+                </div>
+            </footer>
+        </div>
+    </body>
+    </html>
+`;
+
+export default Index;
--- webpack.config.js
+++ webpack.config.js
@@ -1,107 +1,144 @@
-const path = require('path')
-const webpack = require('webpack')
-const CopyPlugin = require('copy-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-const WorkboxPlugin = require('workbox-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const webpack = require("webpack");
+const CopyPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { WebpackPluginServe } = require("webpack-plugin-serve");
+
+const argv = require("webpack-nano/argv");
+const { serve, watch, nodeEnv } = argv;
+
+// Try the environment variable, otherwise use root
+const NODE_ENV = (nodeEnv || process.env.NODE_ENV);
+const PUBLIC_PATH = process.env.PUBLIC_PATH || "/";
+
+const devMode = NODE_ENV === "development";
+const staticPath = path.resolve(__dirname, devMode ? ".cache" : "dist");
 
 const plugins = [
-  new webpack.DefinePlugin({
-    'process.env': JSON.stringify(process.env)
-  }),
+  // new webpack.DefinePlugin({
+  //   "process.env": JSON.stringify(process.env)
+  // }),
   new CopyPlugin({
     patterns: [
-      '.htaccess',
-      'manifest/**/*',
-      'favicon.ico',
-      'manifest.json',
-      'robots.txt'
+      ".htaccess",
+      "manifest/**/*",
+      "favicon.ico",
+      "manifest.json",
+      "robots.txt"
     ],
     options: {
       concurrency: 100
     }
   }),
   new MiniCssExtractPlugin({
-    filename: 'styles/[name].[contenthash].css',
-    chunkFilename: 'styles/[id].css'
+    filename: "styles/[name].[contenthash].css",
+    chunkFilename: "styles/[id].css"
   }),
   new HtmlWebpackPlugin({
-    filename: 'index.html',
-    template: './index.html'
+    compress: true,
+    filename: `index.html`,
+    template: "./index.html.js"
   })
-]
+];
+
+const Serve = () => new WebpackPluginServe({
+  host: "localhost",
+  compress: true,
+  historyFallback: true,
+  liveReload: true,
+  publicPath: PUBLIC_PATH,
+  static: staticPath,
+  open: true
+});
+
+const Terser = () => new TerserPlugin({
+  parallel: true,
+  terserOptions: {
+    format: {
+      comments: false
+    },
+    mangle: false,
+    keep_classnames: true,
+    keep_fnames: true,
+    // browser fixes
+    ie8: true,
+    safari10: true
+  },
+  extractComments: false
+});
+
+const entryPoints = [
+  "./scripts/main.js",
+  "./styles/main.scss"
+];
 
 const configDefault = {
-  context: path.join(__dirname, 'src'),
-  entry: [
-    './scripts/main.js',
-    './styles/main.scss'
-  ],
+  context: path.join(__dirname, "src"),
+  entry: devMode && serve
+    ? [
+      "webpack-plugin-serve/client",
+      ...entryPoints
+    ]
+    : entryPoints,
   module: {
     rules: [
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: { filename: 'fonts/[name].[contenthash].[ext]' }
+        type: "asset/resource",
+        generator: { filename: "fonts/[name].[contenthash].[ext]" }
       },
       {
         test: /\.(png|svg|jpe?g|webp|gif|ico)$/i,
-        type: 'asset/resource',
-        generator: { filename: 'images/[name].[contenthash].[ext]' }
+        type: "asset/resource",
+        generator: { filename: "images/[name].[contenthash].[ext]" }
       },
       {
         test: /\.(sa|sc|c)ss$/i,
         use: [
-          process.env.NODE_ENV === 'development'
-            ? 'style-loader'
+          process.env.NODE_ENV === "development"
+            ? "style-loader"
             : MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               importLoaders: 1,
               sourceMap: true
             }
           },
           {
-            loader: 'sass-loader'
+            loader: "sass-loader"
           }
         ]
       }
     ]
   },
-  output: {
-    filename: 'scripts/[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist')
+  optimization: {
+    minimize: Boolean(devMode),
+    minimizer: devMode ? [] : [Terser(), new CssMinimizerPlugin()],
+    usedExports: devMode
   },
-  target: 'web'
-}
+  output: {
+    filename: `scripts/${devMode ? "[name]" : `[name]-[contenthash]`}.js`,
+    chunkFilename: `scripts/${devMode ? "[id]" : `[id]-[contenthash]`}.chunk.js`,
+    path: staticPath,
+    publicPath: PUBLIC_PATH
+  },
+  target: "web",
+  watch: devMode && (watch || serve)
+};
 
 const config = {
   development: {
-    mode: 'development',
-    devtool: 'source-map',
-    devServer: {
-      open: true,
-      static: {
-        directory: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-        serveIndex: true
-      },
-      historyApiFallback: true,
-      compress: true
-    },
-    optimization: {},
-    plugins: plugins.concat([])
+    mode: "development",
+    devtool: "cheap-module-source-map",
+    plugins: plugins.concat(devMode && serve ? [Serve()] : [])
   },
   production: {
-    mode: 'production',
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin({}), '...', new CssMinimizerPlugin()],
-      usedExports: true
-    },
+    mode: "production",
     plugins: plugins.concat([
       new WorkboxPlugin.GenerateSW({
         clientsClaim: true,
@@ -109,11 +146,9 @@ const config = {
       })
     ])
   }
-}
+};
 
-module.exports = (env, argv) => {
-  return ({
-    ...configDefault,
-    ...config[argv.mode || 'production']
-  })
-}
+module.exports = {
+  ...configDefault,
+  ...config[NODE_ENV || "production"]
+};
